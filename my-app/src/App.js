2ddb1f152535b50b82df770d5445ac5f2632246e
Junior-Garces <Junior-Garces>
2024-04-10 11:51:24 -0500

    vercion estable 6

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -37,77 +37,7 @@ function App() {
             <Route exact path="/Home" element={<Home />} />
             <Route path="/productos" element={<Productos />} />
           </Routes>
-          <div className="page-container">
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto1.jpg" alt="ImgProducto" />
-              <h2>titutulo1</h2>
-              <p> descripción1</p>
-              <li><Link to="/productos">Productos</Link></li>
 
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto2.jpg" alt="ImgProducto" />
-              <h2>titutulo2</h2>
-              <p> descripción2</p>
-              <li><Link to="/productos">Productos</Link></li>
-
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto3.jpg" alt="ImgProducto" />
-              <h2>titutulo3</h2>
-              <p> descripción3</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto4.jpg" alt="ImgProducto" />
-              <h2>titutulo4</h2>
-              <p> descripción4</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto5.jpg" alt="ImgProducto" />
-              <h2>titutulo5</h2>
-              <p> descripción5</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto6.jpg" alt="ImgProducto" />
-              <h2>titutulo6</h2>
-              <p> descripción6</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto7.jpg" alt="ImgProducto" />
-              <h2>titutulo7</h2>
-              <p> descripción7</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto8.jpg" alt="ImgProducto" />
-              <h2>titutulo8</h2>
-              <p> descripción8</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto9.jpg" alt="ImgProducto" />
-              <h2>titutulo9</h2>
-              <p> descripción9</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto10.jpg" alt="ImgProducto" />
-              <h2>titutulo10</h2>
-              <p> descripción10</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto11.jpg" alt="ImgProducto" />
-              <h2>titutulo11</h2>
-              <p> descripción11</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto1.jpg" alt="ImgProducto" />
-              <h2>titutulo12</h2>
-              <p> descripción12</p>
-            </div>
-            <div className="container">
-              <img src="/imagenes/imgProductos/ImgProducto2.jpg" alt="ImgProducto" />
-              <h2>titutulo13</h2>
-              <p> descripción13</p>
-            </div>
-          </div>
           <div className="containerWhatsapp">
             <a href="https://wa.me/+573022573054">
               <img src="/imagenes/WhatsApp_Logo.jpg" alt="img whatsapp" />
